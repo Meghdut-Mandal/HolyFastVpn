e7a0b64880041e9b3bddbb6baa1ca3d43df215a9
Meghdut Mandal <meghdut.windows@gmail.com>
2020-12-19 15:05:01 +0530

    TODO logout issue

--- app/src/main/java/com/holy/fast/vpn/ui/ui/DashboardFragment.kt
+++ app/src/main/java/com/holy/fast/vpn/ui/ui/DashboardFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.holy.fast.vpn.R
 import com.holy.fast.vpn.ui.AuthActivity
@@ -28,8 +30,8 @@ class DashboardFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         log_out.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-            val intent= Intent(requireActivity(),AuthActivity::class.java)
+            Firebase.auth.signOut()
+            val intent = Intent(requireActivity(), AuthActivity::class.java)
             startActivity(intent)
             requireActivity().finishAffinity()
         }
